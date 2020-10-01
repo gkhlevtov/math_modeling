6846a94f71525fbad7c74f05032a81031687fd03
gkhlevtov <71844829+gkhlevtov@users.noreply.github.com>
2020-10-01 18:11:22 +0200

    Практикум №2 Задача 5

--- lab_ex5.py
+++ lab_ex5.py
@@ -3,7 +3,8 @@ if b == 0:
     print('Ошибка')
 elif a % b == 0:
     print('Число', a, 'делится на число', b)
-    print(a % b)
+    print('Остаток', a % b)
     print(a, '/', b, '=', int(a / b))
 else:
+    print('Остаток', a % b)
     print(a, '/', b, '=', a / b)
